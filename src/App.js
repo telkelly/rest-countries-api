9f343a8ea23e47d3435181c9374baa37709ac809
Kelly <84016657+telkelly@users.noreply.github.com>
2022-07-14 13:55:14 +0300

    did header and filter component and set css properties for them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Countries from './components/Countries'
+import Filter from './components/Filter'
+import Header from './components/Header'
   
 export default function App() {
   return (
     <>
+      <Header />
+      <Filter />
       <Countries />
     </>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+      <>
+          <header className='header'>
+              <h1>Where in the world?</h1>
+              <div>
+                  <i className="fas fa-moon"></i><p>Dark mode</p>
+              </div>
+          </header>
+      </>
+  )
+}
